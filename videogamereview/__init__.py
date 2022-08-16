4bbefe668305bc994bf0d7cedfa5c651da4dd5e9
joshfreeman00 <josh2000freeman@aol.co.uk>
2022-08-16 14:37:13 +0000

    Include code to connect to mongoDB

--- videogamereview/__init__.py
+++ videogamereview/__init__.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_pymongo import PyMongo
 if os.path.exists("env.py"):
     import env  # noqa
 
@@ -8,7 +9,10 @@ if os.path.exists("env.py"):
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 
 db = SQLAlchemy(app)
+mongo = PyMongo(app)
 
 from videogamereview import routes  # noqa
