dcdd39559908e9b2024053e515ce159837672c42
joshfreeman00 <josh2000freeman@aol.co.uk>
2022-08-17 13:41:10 +0000

    Start route for adding games

--- videogamereview/routes.py
+++ videogamereview/routes.py
@@ -7,4 +7,23 @@ from videogamereview.models import Game, User
 
 @app.route("/")
 def home():
-    return render_template("base.html")
+    return render_template("reviews.html")
+
+
+@app.route("/games")
+def games():
+    return render_template("games.html")
+
+
+@app.route("/add_game", methods=["GET", "POST"])
+def add_game():
+    if request.method == "POST":
+        game = Game(
+            game_name=request.form.get("game_name"),
+            developer=request.form.get("developer"),
+            genre=request.form.get("game_name"),
+            release_year=Int(request.form.get("release_year")),
+            game_description=request.form.get("game_description")
+        )
+        db.session.add(game)
+        db.session.commit()
