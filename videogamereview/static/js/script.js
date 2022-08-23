de4302c5b38f79e7922d577c5380b6c9827012c8
joshfreeman00 <josh2000freeman@aol.co.uk>
2022-08-23 13:42:54 +0000

    Implement defensive programming

--- videogamereview/static/js/script.js
+++ videogamereview/static/js/script.js
@@ -2,4 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
     // sidenav initialization
     let sidenav = document.querySelectorAll(".sidenav");
     M.Sidenav.init(sidenav);
+
+    let modal = document.querySelectorAll('.modal');
+    M.Modal.init(elems, options);
 });
