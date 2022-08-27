9ec30832ab2f745dcb11cb1a4595440203d81336
joshfreeman00 <josh2000freeman@aol.co.uk>
2022-08-27 15:42:49 +0000

    Implement select initialization

--- videogamereview/static/js/script.js
+++ videogamereview/static/js/script.js
@@ -2,7 +2,10 @@ document.addEventListener("DOMContentLoaded", function() {
     // sidenav initialization
     let sidenav = document.querySelectorAll(".sidenav");
     M.Sidenav.init(sidenav);
-
+    // modal initialization
     let modal = document.querySelectorAll('.modal');
     M.Modal.init(modal);
+    // form select initialization
+    let select = document.querySelectorAll('select');
+    M.FormSelect.init(select);
 });
